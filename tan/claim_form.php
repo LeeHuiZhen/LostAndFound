6b54199aa494912426a7e6bbe982eb45854f1cae
Lee Hui Zhen <zhen050103@gmail.com>
2026-06-24 11:39:18 +0800

    Update claim_form.php

--- tan/claim_form.php
+++ tan/claim_form.php
@@ -16,7 +16,7 @@ if (session_status() === PHP_SESSION_NONE) {
 }
 
 if (!isset($_SESSION['user_id'])) {
-    header("Location: /tey/login/login.php");
+    header("Location: /tey/login.php");
     exit();
 }
 
